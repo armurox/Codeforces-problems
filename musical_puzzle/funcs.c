289a04a8fd57132bd44adc7a295a0c8fc459229e
Arman Jasuja <arman_jasuja@yahoo.com>
2023-05-28 12:28:16 +0400

    Submitted, runtime error present

--- musical_puzzle/funcs.c
+++ musical_puzzle/funcs.c
@@ -8,20 +8,29 @@ void min_num(char *arr, int size)
     char *used_arr = calloc((size), sizeof(char));
     int total_num = 0;
     int x = 0;
+    int check = 0;
     for (int i = 0; arr[i + 1] != '\0'; i++)
     {
-        // printf("test1\n");
+        // Check if current melody is in used array
         for (int j = 0; j < size; j += 2)
         {
             if (arr[i] == used_arr[j] && arr[i + 1] == used_arr[j + 1])
             {
-                // printf("test\n");
+                check++;
+                break;
             }
         }
 
-        used_arr[x] = arr[i];
-        used_arr[x + 1] = arr[i + 1];
-        x += 2;
+        // Increment number of melodies required and add to used array
+        if (check == 0)
+        {
+            used_arr[x] = arr[i];
+            used_arr[x + 1] = arr[i + 1];
+            x += 2;
+            total_num++;
+        }
+
+        check = 0;
     }
 
     printf("%d\n", total_num);
