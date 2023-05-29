1b4a3035708d68fc720e407a3c15f73cd3253bec
Arman Jasuja <arman_jasuja@yahoo.com>
2023-05-29 11:14:07 +0400

    Scanf error fixed

--- restore_the_weather/restore.c
+++ restore_the_weather/restore.c
@@ -6,28 +6,28 @@ void print_correct(int *expected_temp, int *act_temp, int size, int allowed_diff
 int main()
 {
     // Get number of test cases from user
-    int case_num;
-    scanf("%d", &case_num);
+    // int case_num;
+    // scanf("%d ", &case_num);
 
-    for (int i = 0; i < case_num; i++)
-    {
+    // for (int i = 0; i < case_num; i++)
+    // {
         // Get array size and max difference between expected and actual temperature from user
         int arr_size;
         int k;
-        scanf("%d %d", arr_size, k);
+        scanf("%d %d", &arr_size, &k);
 
         // Get expected temp array
-        int *expected_temp = malloc(sizeof(int) * arr_size);
+        int *expected_temp = malloc(arr_size * sizeof(int));
         for (int j = 0; j < arr_size; j++)
         {
-            scanf("%d ", expected_temp + j);
+            scanf("%d", expected_temp + j);
         }
 
         // Get actual temp array
         int *act_temp = malloc(sizeof(int) * arr_size);
         for (int j = 0; j < arr_size; j++)
         {
-            scanf("%d ", act_temp + j);
+            scanf("%d", act_temp + j);
         }
 
         // Print out the correct actual air temp value
@@ -35,7 +35,7 @@ int main()
 
         free(expected_temp);
         free(act_temp);
-    }
+    // }
 
 }
 
@@ -71,4 +71,10 @@ void print_correct(int *expected_temp, int *act_temp, int size, int allowed_diff
             }
         }
     }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", act_temp[i]);
+    }
+    printf("\n");
 }
