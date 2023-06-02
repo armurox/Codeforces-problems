adb670cf0520b4f9bfb66a90e7fc7fbf53014c6d
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-02 09:02:04 +0400

    Solving new problem- incomplete atm

---
+++ permutation_swap/permutation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Get number of cases from user
+    int case_num;
+    scanf("%d", &case_num);
+    
+    for (int i = 0; i < case_num; i++)
+    {
+        int arr_size;
+        scanf("%d", &arr_size);
+        int *arr = malloc(sizeof(int) * arr_size);
+        for (int j = 0; j < arr_size; j++)
+        {
+            scanf("%d", arr + j);
+        }
+
+        int swaps = 1;
+        int num_sweeps = 0;
+        for (int k = 2; k < arr_size; k++)
+        {
+            while (swaps > 0)
+            {
+                num_sweeps++;
+                swaps = 0;
+                for (int l = 0; l < arr_size; l++)
+                {
+
+                }
+            }
+        }
+    }
+}
