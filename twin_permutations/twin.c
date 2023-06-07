5af7d5bc88adda6bdaebb5f8ac1a03c0ff4b92ae
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-07 08:44:04 +0400

    New problem, solution being adjusted

---
+++ twin_permutations/twin.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int size;
+        scanf("%d", &size);
+        int *init = malloc(sizeof(int) * size);
+        for (int j = 0; j < size; j++)
+        {
+            scanf("%d", init + i);
+        }
+
+        int *reverse = malloc(sizeof(int) * size);
+        for (int j = 0; j < size; j++)
+        {
+            reverse[j] = init[size - 1 - j];
+        }
+
+        int *sum_arr = malloc(sizeof(int) * size);
+        int *b = malloc(sizeof(int) * size);
+        for (int j = 0; j < size; j++)
+        {
+            sum_arr[j] = size - j;
+        }
+
+        for (int j = 0; j < size; j++)
+        {
+            if (j == 0)
+            {
+               b[j] = reverse[j] + sum_arr[j];
+               sum_arr[j] = 40000;
+               continue;
+            }
+
+            for (int k = 1; k < size; k++)
+            {
+                if (reverse[j] + sum_arr[k] <= b[j - 1])
+                {
+                    b[j] = reverse[j] + sum_arr[k];
+                    sum_arr[k] = 40000;
+                }
+            }
+        }
+        
+        // Remember to reverse b once done!
+        int *b_rev = malloc(sizeof(int) * size);
+        for (int j = 0; j < size; j++)
+        {
+            b_rev[j] = b[size - 1 - j];
+        }
+
+        free(b);
+        free(reverse);
+        free(sum_arr);
+        free(init);
+        for (int j = 0; j < size; j++)
+        {
+            printf("%d ", b_rev[j]);
+        }
+        printf("\n");
+        free(b_rev);
+    }
+}
