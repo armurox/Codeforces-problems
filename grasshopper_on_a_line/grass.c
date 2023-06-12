17ea1523cd31784fb6b15d422ca72e4269469caa
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-12 08:38:48 +0530

    Solution accepted

--- grasshopper_on_a_line/grass.c
+++ grasshopper_on_a_line/grass.c
@@ -4,5 +4,24 @@
 
 int main()
 {
-    
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int x;
+        int k;
+        scanf("%d %d", &x, &k);
+        int num_jumps;
+        if (x % k > 0)
+        {
+            num_jumps = 1;
+            printf("%d\n%d\n", num_jumps, x);
+        }
+
+        else
+        {
+            num_jumps = 2;
+            printf("%d\n%d %d\n", num_jumps, x - 1, 1);
+        }
+    }
 }
