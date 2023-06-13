693b68b199b8deabe69bbd0079a95539d95daf5b
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-13 08:50:51 +0530

    Wrong answer in test 2

--- blackboard_list/blackboard.c
+++ blackboard_list/blackboard.c
@@ -4,5 +4,35 @@
 
 int main()
 {
-    
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int size;
+        scanf("%d", &size);
+        int *arr = malloc(sizeof(int) * size);
+        for (int j = 0; j < size; j++)
+        {
+            scanf("%d", arr + j);
+        }
+
+        for (int j = 0; j < size - 1; j++)
+        {
+            for (int k = 1; k < size; k++)
+            {
+                for (int l = 0; l < size; l++)
+                {
+                    int test = abs(arr[j] - arr[k]);
+                    if (test == arr[l])
+                    {
+                        printf("%d\n", arr[j]);
+                        goto jump;
+                    }
+                }
+                
+            }
+        }
+        jump:
+            continue;
+    }
 }
