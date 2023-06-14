17877612ea639e268735a1f65f74a11865334cc8
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-14 08:59:27 +0530

    Solution not accepted

--- the_good_array/good.c
+++ the_good_array/good.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int main()
 {
@@ -8,6 +9,22 @@ int main()
     scanf("%d", &case_num);
     for (int i = 0; i < case_num; i++)
     {
-        
+        int n, k;
+        scanf("%d %d", &n, &k);
+        int min_ones = 2;
+        for (int j = 1; j <= n; j++)
+        {
+            if (ceil(j / k) > min_ones && j <= n / 2)
+            {
+                min_ones = 2 * ceil(j / k);
+            }
+
+            else if (j > ceil(n / 2) && min_ones < n)
+            {
+                min_ones = 2 * ceil(j / k);
+            }
+        }
+
+        printf("%d\n", min_ones);
     }
 }
