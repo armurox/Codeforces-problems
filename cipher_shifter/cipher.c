40136a370db75cf1d5e3bcb93d6674d0d337465b
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-14 09:48:43 +0530

    Solution Accepted

--- cipher_shifter/cipher.c
+++ cipher_shifter/cipher.c
@@ -7,6 +7,32 @@ int main()
     scanf("%d", &case_num);
     for (int i = 0; i < case_num; i++)
     {
-        
+        int size;
+        scanf("%d", &size);
+        char *arr = calloc(size, sizeof(char));
+        char *orig_arr = calloc(size, sizeof(char));
+        for (int j = 0; j < size; j++)
+        {
+            scanf(" %c", arr + j);
+        }
+
+        char orig = arr[0];
+        int k = 0;
+        for (int j = 1; j < size; j++)
+        {
+            if (arr[j] == orig)
+            {
+                orig_arr[k] = orig;
+                orig = arr[j + 1];
+                j++;
+                k++;
+            }
+        }
+
+        for (int j = 0; orig_arr[j] != '\0'; j++)
+        {
+            printf("%c", orig_arr[j]);
+        }
+        printf("\n");
     }
 }
