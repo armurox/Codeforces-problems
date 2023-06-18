6a9dd3e77a1173c4d03b43b65c7ab4101593adf0
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-18 08:35:43 +0530

    New problem started

---
+++ game_with_board/game.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n < 5)
+        {
+            printf("Bob\n");
+        }
+
+        else 
+        printf("Alice\n");
+    }
+}
