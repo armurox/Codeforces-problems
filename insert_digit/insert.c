7dd105763f72b39dd0848ae851893316c62e776f
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-19 08:23:12 +0400

    New Problem Started

---
+++ insert_digit/insert.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        
+    }
+}
