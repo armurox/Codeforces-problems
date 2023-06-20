c6ca7c7cfd07ece26576519cd16762b362f27791
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-20 09:06:14 +0400

    Still working on problem logic

--- candies/candies.py
+++ candies/candies.py
@@ -1,12 +1,22 @@
 def main():
     case_num = int(input())
+    moves = []
     for _ in range(case_num):
         num_candies = int(input())
         if num_candies % 2 == 0:
             print(-1)
             continue
+        x = 1
+        num_moves = 0
+        while 2 * x + 1 < num_candies:
+            x = 2 * x + 1
+            num_moves +=1
+            moves.append(2)
         
-    
+        
+            
+                
+            
     
     
     
