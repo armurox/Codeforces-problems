301c14dac9fa17665d7f6e993e089510970601ee
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-20 10:49:59 +0400

    Python Solution Accepted

--- candies/candies.py
+++ candies/candies.py
@@ -1,20 +1,47 @@
+num_moves = 2
 def main():
     case_num = int(input())
-    moves = []
     for _ in range(case_num):
         num_candies = int(input())
         if num_candies % 2 == 0:
             print(-1)
             continue
-        x = 1
-        num_moves = 0
-        while 2 * x + 1 < num_candies:
-            x = 2 * x + 1
-            num_moves +=1
+        x = 3
+        moves = [2]
+        create_arr(x, moves, num_candies)
+        print(num_moves)
+        for move in moves:
+            print(move, end = " ")
+        print()
+
+def create_arr(x, moves, num_candies):
+    global num_moves
+    if num_candies == 3:
+        num_moves = 1
+        return
+    if num_candies == 7:
+        num_moves = 2
+        if x == 3:
             moves.append(2)
-        
-        
+        return 2
+    elif num_candies == 5:
+        num_moves = 2
+        if x == 3:
+            moves.append(1)
+        return 1
+    if ((num_candies - 1) / 2) % 2 != 0:
+        if create_arr(x + 1, moves, (num_candies - 1) / 2) != None:
+            moves.append(create_arr(x + 1, moves, (num_candies - 1) / 2))
+        moves.append(2)
+        num_moves += 1
+    elif ((num_candies + 1)/ 2) % 2 != 0:
+        if create_arr(x + 1, moves, (num_candies + 1) / 2) != None:
+            moves.append(create_arr(x + 1, moves, (num_candies + 1) / 2))
+        moves.append(1)
+        num_moves += 1
+    
             
+        
                 
             
     
@@ -22,5 +49,5 @@ def main():
     
     
     
-if __name__ = "main":
+if __name__ == "__main__":
     main()
