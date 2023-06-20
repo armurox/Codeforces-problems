f4c29cf6ad76037d44b4d2b833d25d13f63c54da
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-20 08:59:36 +0400

    Python attempt added

---
+++ candies/candies.py
@@ -0,0 +1,16 @@
+def main():
+    case_num = int(input())
+    for _ in range(case_num):
+        num_candies = int(input())
+        if num_candies % 2 == 0:
+            print(-1)
+            continue
+        
+    
+    
+    
+    
+    
+    
+if __name__ = "main":
+    main()
