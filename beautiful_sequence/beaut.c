8afac0891f71b8dac2cdb9a49ad545f534f02dc4
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-23 08:05:46 +0400

    Solution Accepted

--- beautiful_sequence/beaut.c
+++ beautiful_sequence/beaut.c
@@ -8,6 +8,35 @@ int main()
     scanf("%d", &case_num);
     for (int i = 0; i < case_num; i++)
     {
-        
+        int size;
+        scanf("%d", &size);
+        int *arr = malloc(sizeof(int) * size);
+        for (int j = 0; j < size; j++)
+        {
+            scanf("%d", arr + j);
+        }
+
+        int yes = 0;
+        for (int j = 0; j < size; j++)
+        {
+            for (int k = j; k < size; k++)
+            {
+                if (arr[k] == j + 1)
+                {
+                    printf("YES\n");
+                    yes = 1;
+                    break;
+                }
+            }
+            if (yes == 1)
+            {
+                break;
+            }
+        }
+
+        if (yes != 1)
+        {
+            printf("NO\n");
+        }
     }
 }
