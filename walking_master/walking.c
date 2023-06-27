e17700cff980137c7de4246be2ae4b8408676071
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-27 09:15:25 +0400

    Solution Accepted

--- walking_master/walking.c
+++ walking_master/walking.c
@@ -8,6 +8,34 @@ int main()
     scanf("%d", &case_num);
     for (int i = 0; i < case_num; i++)
     {
-        
+        int a, b, c, d;
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        int num_moves = 0;
+
+        if (b > d)
+        {
+            printf("%d\n", -1);
+            continue;
+        }
+        while (b != d)
+        {
+            a++;
+            b++;
+            num_moves++;
+        }
+
+        if (a < c)
+        {
+            printf("%d\n", -1);
+            continue;
+        }
+
+        while (a != c)
+        {
+            a--;
+            num_moves++;
+        }
+        printf("%d\n", num_moves);
+
     }
 }
