a1a56829bc5f205cc45e6f7602bb0181d7cd23b8
Arman Jasuja <arman_jasuja@yahoo.com>
2023-06-29 10:34:17 +0400

    Solution Accepted

--- sasha_and_array_coloring/sasha.py
+++ sasha_and_array_coloring/sasha.py
@@ -1,4 +1,17 @@
 def main():
+    case_num = int(input())
+    for _ in range(case_num):
+        size = int(input())
+        nums = input().split()
+        nums = [eval(i) for i in nums]
+        cost = 0
+        while len(nums) > 1:
+            max_num = max(nums)
+            min_num = min(nums)
+            cost += max_num - min_num
+            nums.remove(max_num)
+            nums.remove(min_num)
+        print(cost)
     
     
     
