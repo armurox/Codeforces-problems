85d64baa7aa01ae959990de3d3c98c849301272b
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-08 12:00:29 +0400

    Still not passing test 2

--- recent_actions/recent.py
+++ recent_actions/recent.py
@@ -7,22 +7,25 @@ def main():
         later_acts = input().split()
         later_acts = [eval(i) for i in later_acts]
         num_filled = 0
-        count = 1
-        num_kicked = {}
+        num_moves = 0
+        num_moves_for_removed = []
         recent_acts = {}
-        for i, val in enumerate(later_acts):
-            if val not in recent_acts:
+        for val in later_acts:
+            if val in recent_acts:
+                recent_acts[val] += 1
+                num_moves += 1          
+            else:
                 recent_acts[val] = 1
-                num_kicked[i + 1] = n - count
                 num_filled += 1
+                num_moves += 1
+                num_moves_for_removed.append(num_moves)
             if num_filled == n:
                 break
-            count += 1
         
         for _ in range(n - num_filled):
             print(-1, end = " ")
         
-        for i in reversed(num_kicked):
+        for i in reversed(num_moves_for_removed):
             print(i, end = " ")
         print()
     
