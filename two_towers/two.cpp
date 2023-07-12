9dc2492e22087d6612841f19b7e66b7884bbf38b
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-12 11:37:03 +0400

    test 1 passed

--- two_towers/two.cpp
+++ two_towers/two.cpp
@@ -14,24 +14,37 @@ int main()
         string f;
         string s;
         cin >> f >> s;
-        int fdb = f.find("BB");
-        int fdr = f.find("RR");
-        int ftb = f.find("BBB");
-        int ftr = f.find("RRR");
-        int sdb = s.find("BB");
-        int sdr = s.find("RR");
-        int stb = s.find("BBB");
-        int str = s.find("RRR");
+        size_t fdb = f.find("BB");
+        size_t fdr = f.find("RR");
+        size_t ftb = f.find("BBB");
+        size_t ftr = f.find("RRR");
+        size_t sdb = s.find("BB");
+        size_t sdr = s.find("RR");
+        size_t stb = s.find("BBB");
+        size_t str = s.find("RRR");
+        size_t sub_fdb = s.substr(1, n - 2).find("BB");
+        size_t sub_fdr = s.substr(1, n - 2).find("RR");
+        size_t sub_sdb = s.substr(1, m - 2).find("BB");
+        size_t sub_sdr = s.substr(1, m - 2).find("RR");
         if (ftr != string::npos || ftb != string::npos || str != string::npos || stb != string::npos)
         {
             cout << "NO\n";
         }
 
-        else if ()
+        else if (f[n - 1] == s[m - 1] && (fdr != string::npos || fdb != string::npos || sdr != string::npos || sdb != string::npos))
         {
             cout << "NO\n";
         }
 
+        else if (f[n - 1] != s[m - 1] && ((sub_fdr != string::npos && n != 1) || (sub_fdb != string::npos && n != 1) || (sub_sdr != string::npos && m != 1) || (sub_sdb != string::npos && m != 1)))
+        {
+            cout << "NO\n";
+        }
+
+        else if (f[n - 1] != s[m - 1] && ((fdr != string::npos && sdb != string::npos) || (fdb != string::npos && sdr != string::npos)))
+        {
+            cout << "NO\n";
+        }
         else
         {
             cout << "YES\n";
