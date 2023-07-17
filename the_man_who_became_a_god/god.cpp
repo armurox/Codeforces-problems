0c35ba9711ed086152e16cfeab43d551ed20d38a
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-17 11:07:49 +0400

    Problem still under development

--- the_man_who_became_a_god/god.cpp
+++ the_man_who_became_a_god/god.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
     int case_num;
     cin >> case_num;
+
+    for (int i = 0; i < case_num; i++)
+    {
+        int n;
+        int k;
+        int min_pow = 0;
+        cin >> n >> k;
+        int arr[n];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[j];
+        }
+
+        // WHen k and n are equal, the minimum power of the group is 0, so we ignore that case
+        int num_groups = 0;
+        if (k < n)
+        {
+            while (num_groups < k - 1)
+            {
+                
+                num_groups++;
+            }
+        }
+        
+    }
 }
