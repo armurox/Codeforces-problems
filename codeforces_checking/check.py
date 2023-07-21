78a0a09700ac86e22714e0cf16a789b501aa1369
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-21 08:38:26 +0400

    Python solution added

---
+++ codeforces_checking/check.py
@@ -0,0 +1,23 @@
+def main():
+    case_num = int(input())
+    for _ in range(case_num):
+        let = input()
+        if let in "codeforces":
+            print("YES")
+        else:
+            print("NO")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
