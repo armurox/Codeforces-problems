128f1a9fc325ad25a4eb65f6e9623296c3e39dbc
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-23 07:45:27 +0530

    Python version of solution added

---
+++ subtraction_game/subtraction.py
@@ -0,0 +1,22 @@
+def main():
+    case_num = int(input())
+    for _ in range(case_num):
+        a, b = input().split()
+        a = int(a)
+        b = int(b)
+        print(a + b)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
