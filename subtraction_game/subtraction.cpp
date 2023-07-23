142387663a6b6f4cb316e49c434bc7669d325f32
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-23 07:45:07 +0530

    New problem- Solution Accepted

---
+++ subtraction_game/subtraction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+}
