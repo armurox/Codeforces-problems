fd579126a24c43a70678bc06e2a9ef1e52cdb9a7
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-25 10:34:34 +0530

    New problem started

---
+++ serval_and_inversion_magic/serval.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int size;
+        scanf("%d", &size);
+        char *arr = (char*) malloc(size + 1);
+        scanf("%s", arr);
+        for (int j = 0; j < size / 2; j++)
+        {
+            if 
+        }
+        free(arr);
+    }
+}
