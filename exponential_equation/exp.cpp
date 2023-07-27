535b0f2c7090b27f3ec1d34f8593d6a3de1e92ee
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-27 07:56:56 +0530

    New Problem Started

---
+++ exponential_equation/exp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n % 2 == 1)
+        {
+            printf("%d\n", -1);
+        }
+
+        else
+        {
+            
+        }
+    }
+}
