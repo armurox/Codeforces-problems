eed236a9c0e0764a6162df1ebb8fb180be6c6bb9
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-28 14:11:43 +0530

    New problem started

---
+++ alternating_deck_hard/alternate.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        int num_greatest = floor((float) (-1 / 2) + sqrt(float(1/4) + (2 * n)));
+        int double_sum = (2 * num_greatest) - 1;
+        int amt_left = n - ((num_greatest *(num_greatest + 1)) / 2);
+        int a = 0;
+        if (((double_sum - 1) % 8) != 0 && ((double_sum - 5) % 8) != 0)
+        {
+            double_sum = (2 * num_greatest) + 1;
+            if ((double_sum - 1) % 8 == 0)
+            {
+                a = 1;
+            }
+            amt_left += num_greatest;
+            double_sum = (2 * num_greatest) - 3;
+        }
+        printf("%d\n", double_sum);
+        int b_sum;
+        int white_b;
+        int black_b;
+        int a_sum;
+        int white_a;
+        int black_a;
+        if (((double_sum - 1) % 8) == 0 || a == 1)
+        {
+            a_sum = (((double_sum - 1) / 8) + 1) * (1 + double_sum);
+            white_a = ceil((float) a_sum / 2);
+            black_a = a_sum - white_a;
+            b_sum = n - a_sum;
+            white_b = floor((float)b_sum / 2);
+            black_b = b_sum - white_b;
+        }
+
+        else
+        {
+            b_sum = (((double_sum - 5) / 8) + 1) * (5 + double_sum);
+            white_b = floor((float) b_sum / 2);
+            black_b = b_sum - white_b;
+            a_sum = n - b_sum;
+            white_a = ceil((float)a_sum / 2);
+            black_a = a_sum - white_a;
+            
+        }
+
+        printf("%d %d %d %d\n", white_a, black_a, white_b, black_b);
+    }
+}
