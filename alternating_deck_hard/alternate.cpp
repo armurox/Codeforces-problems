631dfc7ffb80ba81cb274446d12c54f1644de381
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-28 16:01:28 +0530

    Working on solution

--- alternating_deck_hard/alternate.cpp
+++ alternating_deck_hard/alternate.cpp
@@ -14,12 +14,21 @@ int main()
     {
         int n;
         scanf("%d", &n);
-        int num_greatest = floor((float) (-1 / 2) + sqrt(float(1/4) + (2 * n)));
+        int num_greatest = floor((double) (-0.5) + sqrt((double)(0.25) + (2 * n)));
         int double_sum = (2 * num_greatest) - 1;
         int amt_left = n - ((num_greatest *(num_greatest + 1)) / 2);
         int amt_left_b = 0;
         int amt_left_a = 0;
-        if (((double_sum - 1) % 8) != 0 && ((double_sum - 5) % 8) != 0)
+        if (((double_sum - 1) % 8) == 0)
+        {
+            amt_left_b = amt_left;
+        }
+
+        else if (((double_sum - 5) % 8) == 0)
+        {
+            amt_left_a = amt_left;
+        }
+        else
         {
             double_sum = (2 * num_greatest) + 1;
             if ((double_sum - 1) % 8 == 0)
@@ -35,8 +44,6 @@ int main()
             }
             double_sum = (2 * num_greatest) - 3;
         }
-        printf("%d\n", double_sum);
-        printf("%d\n", amt_left_b);
         int b_sum;
         int white_b;
         int black_b;
@@ -45,8 +52,17 @@ int main()
         int black_a;
         if (((double_sum - 1) % 8) == 0)
         {
-            a_sum = (((double_sum - 1) / 8) + 1) * (1 + double_sum) + amt_left_a;
-            white_a = ceil((float) a_sum / 2);
+            if (double_sum != 1)
+            {
+                a_sum = (((((double_sum - 1) / 8) + 1) * (1 + double_sum)) / 2) + amt_left_a;
+            }
+
+            else
+            {
+                a_sum = 1 + amt_left_a;
+            }
+            
+            white_a =  (a_sum / 2) + 1;
             black_a = a_sum - white_a;
             b_sum = n - a_sum;
             white_b = floor((float)b_sum / 2);
@@ -55,11 +71,20 @@ int main()
 
         else
         {
-            b_sum = (((double_sum - 5) / 8) + 1) * (5 + double_sum) + amt_left_b;
+            if (double_sum != 5)
+            {
+                b_sum = (((((double_sum - 5) / 8) + 1) * (5 + double_sum)) / 2) + amt_left_b;
+            }
+
+            else
+            {
+                 b_sum = 5 + amt_left_b;
+            }
+            
             white_b = floor((float) b_sum / 2);
             black_b = b_sum - white_b;
             a_sum = n - b_sum;
-            white_a = ceil((float)a_sum / 2);
+            white_a = (a_sum / 2) + 1;
             black_a = a_sum - white_a;
             
         }
