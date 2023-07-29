cc846dee13b7e84deecb29c5dd5426f7d48832a1
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-29 12:39:41 +0530

    New Problem Started

---
+++ vika_and_her_friends/vika.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        
+    }
+}
