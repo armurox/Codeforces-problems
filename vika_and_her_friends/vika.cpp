feba9cdd5d3a4596ad6b14568e38394dd78927f2
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-30 11:49:20 +0530

    Working on solution

--- vika_and_her_friends/vika.cpp
+++ vika_and_her_friends/vika.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <utility>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -14,6 +15,29 @@ int main()
     scanf("%d", &case_num);
     for (int i = 0; i < case_num; i++)
     {
-        
+        int n, m, k;
+        scanf("%d %d %d", &n, &m, &k);
+        int x, y;
+        scanf("%d %d", &x, &y);
+        int x_i, y_i;
+        int no = 0;
+        for (int j = 0; j < k; j++)
+        {
+            scanf("%d %d", &x_i, &y_i);
+            if (abs(x + 1 - x_i) + abs(y - y_i) == 1 && abs(x - x_i) + abs(y + 1 - y_i) == 1)
+            {
+                    no = 1;
+            }
+        }
+
+        if (no)
+        {
+            printf("NO\n");
+        }
+
+        else
+        {
+            printf("YES\n");
+        }
     }
 }
