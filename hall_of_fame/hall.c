77817443c3e812e683e37add064a4feed73b3773
Arman Jasuja <arman_jasuja@yahoo.com>
2023-08-02 11:01:08 +0530

    New problem- Solution Accepted

---
+++ hall_of_fame/hall.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int case_num;
+    scanf("%d", &case_num);
+    for (int i = 0; i < case_num; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        char *arr = calloc(n + 1, sizeof(char));
+        scanf("%s", arr);
+        int yes = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (j < n - 1)
+            {
+                if (arr[j] == 'R' &&  arr[j + 1] == 'L')
+                {
+                    printf("%d\n", 0);
+                    yes = 1;
+                    break;
+                }
+            }
+
+            if (j > 0)
+            {
+                if (arr[j] == 'R' && arr[j - 1] == 'L')
+                {
+                    printf("%d\n", j);
+                    yes = 1;
+                    break;
+                }
+            }
+        }
+
+        if (!yes)
+        {
+            printf("%d\n", -1);
+        }
+
+        free(arr);
+    }
+}
