b79de2e6a4d39f60ef35a4733515fd9e4898c117
Arman Jasuja <arman_jasuja@yahoo.com>
2023-08-11 08:01:39 +0530

    New Problem Started

---
+++ block_towers/block.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int case_num;
+    cin >> case_num;
+    for (int i = 0; i < case_num; i++)
+    {
+        int n;
+        cin >> n;
+        
+    }
+}
