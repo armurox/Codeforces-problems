51d340f5d5924e72388b07d7e76f0f52d0bc985d
Arman Jasuja <arman_jasuja@yahoo.com>
2023-08-20 12:03:36 +0400

    Python Version Of Solution Added

---
+++ doremys_paint/doremy.py
@@ -0,0 +1,10 @@
+def main():
+    case_num = int(input())
+    for _ in range(case_num):
+        n = int(input())
+        skip = input()
+        print(1, n)
+    
+    
+if __name__ == "__main__":
+    main()
