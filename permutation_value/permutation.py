7e4d1ecd5fa8ca24cbd9c4379d6a9f0f3c7de7c7
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-08 08:03:59 +0400

    Python Version Of Solution Added

---
+++ permutation_value/permutation.py
@@ -0,0 +1,12 @@
+def main():
+    cn = int(input())
+    for _ in range(cn):
+        n = int(input())
+        print(1, end = " ")
+        for j in reversed(range(2, n + 1)):
+            print(j, end = " ")
+        print()
+    
+    
+if __name__ == "__main__":
+    main()
