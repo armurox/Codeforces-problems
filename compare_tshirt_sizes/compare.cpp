d1de2e62418387a22b041baac846a24817fc6acb
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-13 10:03:33 +0400

    Solution Accepted

--- compare_tshirt_sizes/compare.cpp
+++ compare_tshirt_sizes/compare.cpp
@@ -20,7 +20,7 @@ int main()
         else if ((find(a.begin(), a.end(), 'L') != a.end() && find(b.begin(), b.end(), 'L') == b.end())
         || (count(a.begin(), a.end(), 'X') >  count(b.begin(), b.end(), 'X') && find(a.begin(), a.end(), 'L') != a.end())
         || (find(a.begin(), a.end(), 'M') != a.end() && find(b.begin(), b.end(), 'S') != b.end())
-        || (count(a.begin(), a.end(), 'X') <  count(b.begin(), b.end(), 'X') && find(a.begin(), a.end(), 'S') != a.end()))
+        || (count(a.begin(), a.end(), 'X') <  count(b.begin(), b.end(), 'X') && find(b.begin(), b.end(), 'S') != b.end()))
         {
             cout << ">\n";
         }
