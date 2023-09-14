a1df14185b07b44b9c2aa199101b0ea0109b2e34
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-14 11:04:47 +0400

    Solution Accepted

--- jumbo_extra_cheese_2/jumbo.cpp
+++ jumbo_extra_cheese_2/jumbo.cpp
@@ -24,13 +24,14 @@ int main()
         if (n == 1)
         {
             per += 2 * maxs.at(0);
+            cout << per << "\n";
             continue;
         }
         sort(maxs.begin(), maxs.end());
 
         for (int j = 0; j < n - 1; j++)
         {
-            if (j < 2 || j == n - 1)
+            if (j < 1)
             {
                 per += maxs.at(j);
             }
@@ -38,6 +39,7 @@ int main()
             per += maxs.at(j + 1) - maxs.at(j);
         }
 
+        per += maxs.at(n - 1);
 
         cout << per << "\n";
     }
