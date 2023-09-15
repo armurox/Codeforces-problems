6759928b21451b6f88a30f3a47216b01f49a5038
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-15 09:34:51 +0400

    New Problem- Solution Accepted

---
+++ factorize_n_+_m/factorize.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cn;
+    cin >> cn;
+    for (int i = 0; i < cn; i++)
+    {
+        int n;
+        cin >> n;
+        cout << n << "\n";
+    }
+}
