ff5ae8034b99f9928c5c034e83b405958dc0289f
Arman Jasuja <arman_jasuja@yahoo.com>
2023-09-20 10:05:48 +0400

    New Problem- Solution Accepted

---
+++ bright_nice_brilliant/bright.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cn;
+    cin >> cn;
+    for (int i = 0; i < cn; i++)
+    {
+        int n;
+        cin >> n;
+        if (n == 1)
+        {
+            cout << 1 << "\n";
+            continue;
+        }
+        for (int j = 0; j < n; j++)
+        {
+            if (j < 2)
+            {
+                for (int k = 0; k < j + 1; k++)
+                {
+                    cout << 1 << " ";
+                }
+                cout << "\n";
+            }
+
+            else
+            {
+                cout << 1 << " ";
+
+                for (int k = 1; k < j; k++)
+                {
+                    cout << 0 << " ";
+                }
+
+                cout << 1 << "\n";
+            }
+        }
+    }
+}
