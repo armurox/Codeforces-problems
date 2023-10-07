e46973fe986c808b85ed5b14a1f7427fb4e9c056
Arman Jasuja <arman_jasuja@yahoo.com>
2023-10-07 09:51:31 +0400

    New Problem- Solution Accepted

---
+++ green_gold_god_array_and_permutation/green.py
@@ -0,0 +1,25 @@
+def main():
+    cn = int(input())
+    for _ in range(cn):
+        n = int(input())
+        a = list(map(int, input().split()))
+        temp = a.copy()
+        p = {}
+        temp.sort(reverse = True)
+        count = 1
+        for elem in temp:
+            if elem in p:
+                p[elem].append(count)
+            else:
+                p[elem] = [count]
+            count += 1
+        
+        for elem in a:
+            perm = p[elem].pop()
+            print(perm, end = " ")
+        print()
+        
+    
+
+if __name__ == "__main__":
+    main()
