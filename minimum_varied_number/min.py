37052e3c28188ca1efde16b97f6b6cacd2f8d161
Arman Jasuja <arman_jasuja@yahoo.com>
2023-10-29 10:40:44 +0400

    New Problem- Solution Accepted

---
+++ minimum_varied_number/min.py
@@ -0,0 +1,26 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        s = int(input())
+        _s = 0
+        nums = []
+        for i in range(9):
+            _s += 9 - i
+            if _s < s:
+                nums.append(9 - i)
+                continue
+            elif _s == s:
+                nums.append(9 - i)
+                break
+            else:
+                _s -= 9 - i
+                break
+        if _s < s:
+            nums.append(s - _s)
+        for num in reversed(nums):
+            print(num, end="")
+        print()
+    
+    
+if __name__ == "__main__":
+    main()
