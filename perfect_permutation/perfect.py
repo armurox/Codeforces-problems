4cec3a2ed21eabfca7aaaf5d5c8b048600de4309
Arman Jasuja <arman_jasuja@yahoo.com>
2023-11-06 10:03:11 +0400

    Python Version Of Solution Added

---
+++ perfect_permutation/perfect.py
@@ -0,0 +1,11 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        for i in range(2, n + 1):
+            print(i, end=" ")
+        print(1)
+    
+    
+if __name__ == "__main__":
+    main()
