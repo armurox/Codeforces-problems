eeb383ce78aabb77b8bdb16ecf4b0f4314509620
Arman Jasuja <arman_jasuja@yahoo.com>
2023-11-08 22:58:29 +0400

    Streamlined Solution

--- difference_operations/difference.py
+++ difference_operations/difference.py
@@ -4,11 +4,8 @@ def main():
         yes = True
         n = int(input())
         nums = list(map(int, input().split()))
-        if nums[1] % nums[0] != 0:
-            print("NO")
-            continue
-        for i in range(2, n):
-            if nums[i] % nums[1] != 0 and nums[i] % nums[0] != 0:
+        for i in range(1, n):
+            if nums[i] % nums[0] != 0:
                 yes = False
                 print("NO")
                 break
