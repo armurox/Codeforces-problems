423f915738e23e8c9937e7210922821844ae0eaa
Arman Jasuja <arman_jasuja@yahoo.com>
2023-11-29 15:24:58 +0400

    Test Case 1 passed- working on others

--- simple_design/simple.py
+++ simple_design/simple.py
@@ -3,20 +3,19 @@ def main():
     for _ in range(t):
         x, k = input().split()
         k = int(k)
-        x = list(map(int, list(x)))
-        amt_to_add = sum(x) % k
-        index = 0
-        for i in range(1, len(x) + 1):
-            amt_to_add -= 9 - x[-i]
-            index = -i
-            if amt_to_add
+        temp = list(map(int, list(x)))
+        x = int(x)
+        _sum = sum(temp)
+        amt_to_add = k - _sum % k
+        if amt_to_add == k:
+            print(x)
+        elif len(temp) == 1 and temp[-1] + amt_to_add >= 10:
+            print(10 + k - 1)
+        elif (temp[-1] + amt_to_add >= 10):
+            print(x + 10 - (_sum % k) - 1)
+        else:
+            print(x + amt_to_add)
         
-        
-            
-        
-            
-        
-    
-    
+
 if __name__=="__main__":
     main()
