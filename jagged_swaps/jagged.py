eb1a19cd9dc2191dca1f7a4caf5d9f042623dc7d
Arman Jasuja <arman_jasuja@yahoo.com>
2023-12-01 09:36:08 +0400

    New Problem- Solution Accepted

---
+++ jagged_swaps/jagged.py
@@ -0,0 +1,13 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        nums = list(map(int, input().split()))
+        if nums[0] != 1:
+            print("NO")
+        else:
+            print("YES")
+    
+    
+if __name__ == "__main__":
+    main()
