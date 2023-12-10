dd117d66caf4ab3845e5947653c3595008cf03e1
Arman Jasuja <arman_jasuja@yahoo.com>
2023-12-10 13:33:00 +0400

    Python Version of the solution added

---
+++ binary_imbalance/binary.py
@@ -0,0 +1,13 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        s = input()
+        if "0" in s:
+            print("YES")
+        else:
+            print("NO")
+        
+    
+if __name__ == "__main__":
+    main()
