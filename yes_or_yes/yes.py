7c6bcff160eef46e4f8c45be52eda9ce2f54c600
Arman Jasuja <arman_jasuja@yahoo.com>
2023-12-19 10:34:45 +0400

    New Problem- Solution Accepted

---
+++ yes_or_yes/yes.py
@@ -0,0 +1,11 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        if (input().upper() == "YES"):
+            print("YES")
+        else:
+            print("NO")
+    
+    
+if __name__ == "__main__":
+    main()
