a165b1389a709290ff034d1fc4e1fb0b09796287
Arman Jasuja <arman_jasuja@yahoo.com>
2024-02-12 07:57:34 +0400

    Python Version Of Solution Added

---
+++ ian_visits_mary/ian.py
@@ -0,0 +1,11 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        x, y = map(int, input().split())
+        print(2)
+        print(x - 1, 1)
+        print(x, y)
+    
+    
+if __name__ == "__main__":
+    main()
