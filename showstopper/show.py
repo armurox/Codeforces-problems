293f04a5cc9b636b6a8e83b74649d5223795a7a2
Arman Jasuja <arman_jasuja@yahoo.com>
2024-02-13 08:35:56 +0400

    New Problem - Solution Accepted

--- showstopper/show.py
+++ showstopper/show.py
@@ -1,29 +1,22 @@
 def main():
-    case_num = int(input())
-    for _ in range(case_num):
-        size = int(input())
-        a = input().split()
-        a = [eval(i) for i in a]
-        b = input().split()
-        b = [eval(i) for i in b]
-        max_a = max(a[:size - 1])
-        max_b = max(b[:size - 1])
-        temp_arr = {a[i]:b[i] for i in range(size - 1)}
-        print(temp_arr)
-        # if (b[size - 1] >= max_b and b[size - 1] >= max_a):
-        #     print("Yes")
-        # elif (a[size - 1] >= max_b and a[size - 1] >= max_a):
-        #     print("Yes")
-        # else:
-        #     print("No")
-    
-    
-    
-    
-    
-    
-    
-    
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+        a_n = a[n - 1]
+        b_n = b[n - 1]
+        yes = True
+        for i in range(n - 1):
+            if (a[i] <= a_n and b[i] <= b_n) or (a[i] <= b_n and b[i] <= a_n):
+                continue
+            else:
+                yes = False
+                print("NO")
+                break
+        if (yes):
+            print("YES")
+
     
 if __name__ == "__main__":
     main()
