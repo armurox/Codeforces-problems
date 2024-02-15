a8f72cb25f44348fe44e7414e4dcace72850bfb4
Arman Jasuja <arman_jasuja@yahoo.com>
2024-02-15 08:48:26 +0400

    New Problem - Solution Accepted

---
+++ xor_mixup/xor.py
@@ -0,0 +1,9 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        print(a[0])
+    
+if __name__ == "__main__":
+    main()
