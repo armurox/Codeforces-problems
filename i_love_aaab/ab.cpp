11b72815eb3ebaa419d87a370df749b00355e334
Arman Jasuja <arman_jasuja@yahoo.com>
2024-04-16 07:58:41 +0400

    Working on solution

---
+++ i_love_aaab/ab.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        string s;
+        cin >> s;
+        int size = s.length();
+        char prev = 'B';
+        bool yes = true;
+        for (int j = 0; j < size; j++)
+        {
+            if ((s[j] == 'B' && prev != 'A') || (j == size - 1 && s[j] != 'B'))
+            {
+                cout << "NO\n";
+                yes = false;
+                break;
+            }
+
+            prev = s[j];
+        }
+
+        if (yes)
+            cout << "YES\n";
+    }
+}
