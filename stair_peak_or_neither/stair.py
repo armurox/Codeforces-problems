15a9e72646a692edee4d85cb821894baa29ed70b
Arman Jasuja <arman_jasuja@yahoo.com>
2024-04-22 07:44:13 +0400

    New Problem - Solution Accepted

---
+++ stair_peak_or_neither/stair.py
@@ -0,0 +1,14 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        a, b, c = map(int, input().split())
+        if a < b < c:
+            print('STAIR')
+        elif a < b > c:
+            print('PEAK')
+        else:
+            print('NONE')
+    
+    
+if __name__ == "__main__":
+    main()
