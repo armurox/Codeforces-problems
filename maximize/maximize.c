d8b2b8edd4739e66634b0e746d823fc3e3efa183
Arman Jasuja <arman_jasuja@yahoo.com>
2024-05-12 09:46:22 +0400

    New Problem - Solution Accepted

---
+++ maximize/maximize.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int gcd (int a, int b);
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int x;
+        scanf("%d", &x);
+
+        printf("%d\n", x - 1);
+    }
+}
