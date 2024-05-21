928f6265e9facab57b8aa1bba44fd91c614f3ce7
Arman Jasuja <arman_jasuja@yahoo.com>
2024-05-21 07:57:37 +0400

    Optimized solution based on inputs

--- deletions_of_two_adjacent_letter/delete.py
+++ deletions_of_two_adjacent_letter/delete.py
@@ -4,17 +4,16 @@ def main():
         s = input()
         c = input()
         no = True
-        size = len(s)
-        if size % 2:   
-            for i in range(size):
-                if s[i] == c:
-                    try:
-                        if len(s[i+1:]) % 2 == 0 or len(s[:i]) % 2 == 0:
-                            print("YES")
-                            no = False
-                            break
-                    except IndexError:
-                        pass
+        size = len(s) 
+        for i in range(size):
+            if s[i] == c:
+                try:
+                    if len(s[i+1:]) % 2 == 0 or len(s[:i]) % 2 == 0:
+                        print("YES")
+                        no = False
+                        break
+                except IndexError:
+                    pass
                 
         if no:
             print("NO")
