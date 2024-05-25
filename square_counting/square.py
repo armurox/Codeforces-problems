c4c84733bc7c23210c2d412d301f85f49984f1b9
Arman Jasuja <arman_jasuja@yahoo.com>
2024-05-25 11:20:08 +0400

    Solution accepted

--- square_counting/square.py
+++ square_counting/square.py
@@ -2,14 +2,7 @@ def main():
     t = int(input())
     for _ in range(t):
         n, s = map(int, input().split())
-        num_squared = n * n
-        for i in range(1, n + 2):
-            if i * num_squared > s:
-                print(i - 1)
-                break
-            elif i * num_squared == s:
-                print(i)
-                break
+        print(s // (n * n))
         
     
     
