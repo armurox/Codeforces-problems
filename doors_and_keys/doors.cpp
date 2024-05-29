4d3d960d6d002d015dec800be40cad51e4722bd2
Arman Jasuja <arman_jasuja@yahoo.com>
2024-05-29 07:38:46 +0400

    New Problem - Solution Accepted

---
+++ doors_and_keys/doors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        string s;
+        cin >> s;
+        if (s.find("r") < s.find("R") && s.find("g") < s.find("G") && s.find("b") < s.find("B"))
+        {
+            cout << "YES\n";
+        }
+
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+}
