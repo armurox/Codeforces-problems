d6f3ecdd50bb52d42e5b1b19572f790487f75390
Arman Jasuja <arman_jasuja@yahoo.com>
2024-06-16 12:20:41 +0400

    C++ version of solution added

---
+++ dual_trigger/dual.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        string l;
+        cin >> l;
+        int num_ones = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (l[j] == '1')
+            {
+                num_ones++;
+            }
+        }
+
+        string ans = (num_ones % 2 || (num_ones == 2 && (l.find("11") != string::npos))) ? "NO\n" : "YES\n";
+        cout << ans;
+    }
+}
