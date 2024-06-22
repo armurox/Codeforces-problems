5365cc815b5877aaac1bfa3e31ac7f1916eabf33
Arman Jasuja <arman_jasuja@yahoo.com>
2024-06-22 11:08:16 +0400

    New Problem - Solution Accepted

---
+++ maximum_multiple_sum/mult.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", (n == 3) ? 3 : 2);
+    }
+}
