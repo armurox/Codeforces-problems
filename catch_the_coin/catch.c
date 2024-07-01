dd9d3ba1856f85706e487dfb1652d5951d89c70b
Arman Jasuja <arman_jasuja@yahoo.com>
2024-07-01 07:47:31 +0400

    New Problem - Solution Accepted

---
+++ catch_the_coin/catch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        printf("%s\n", (y < -1 ) ? "NO" : "YES");
+    }
+}
