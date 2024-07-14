98d9364546218ad128aeb163a0fc772dbcc51004
Arman Jasuja <arman_jasuja@yahoo.com>
2024-07-14 11:04:09 +0400

    New Problem - Solution Accepted

---
+++ only_pluses/only.py
@@ -0,0 +1,18 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        arr = sorted(list(map(int, input().split())))
+        amount_left = 5
+        while amount_left > 0:
+            if arr[1] - arr[0] > amount_left:
+                arr[0] += amount_left
+                amount_left = 0
+            else:
+                amount_left -= (arr[1] - arr[0]) or 1
+                arr[0] += (arr[1] - arr[0]) or 1
+                arr.sort()
+        print(arr[0] * arr[1] * arr[2])
+
+
+if __name__ == "__main__":
+    main()
