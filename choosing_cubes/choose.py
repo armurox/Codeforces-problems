5858646b5079a01437c26d9c03acf7847496b88b
Arman Jasuja <arman_jasuja@yahoo.com>
2024-07-24 09:12:18 +0530

    New Problem - Solution Accepted

---
+++ choosing_cubes/choose.py
@@ -0,0 +1,17 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, f, k = map(int, input().split())
+        a = list(map(int, input().split()))
+        f = a[f - 1]
+        a.sort(reverse=True)
+        c = a.count(f)
+        if f in a[:k]:
+            print('YES') if c <= a[:k].count(f) else print('MAYBE')
+        else:
+            print('NO')
+    
+    
+    
+if __name__ == "__main__":
+    main()
