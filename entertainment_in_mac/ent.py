df8a802210fc71d1918aca4f6e58a81c7eefa936
Arman Jasuja <arman_jasuja@yahoo.com>
2024-07-27 10:53:37 +0530

    New Problem - Solution Accepted

--- entertainment_in_mac/ent.py
+++ entertainment_in_mac/ent.py
@@ -3,21 +3,26 @@ def main():
     for _ in range(t):
         n = int(input())
         s = input()
-        index = False
-        for i in range(n // 2 + 1):
-            if s[i] < s[-(i + 1)]:
-                index = True
+        no_swap = False
+        swap = False
+        size = len(s)
+        if s == s[::-1]:
+            print(s)
+            continue
+        for i in range(size // 2):
+            if (n % 2 == 0 and s[i] < s[size - 1 - i]) or (n % 2 and s[i] > s[size - 1 - i]):
+                no_swap = True
                 break
-        if index:
-            for _ in range(n):
+            elif (n % 2 == 0 and s[i] > s[size - 1 - i]) or (n % 2 and s[i] < s[size - 1 - i]):
+                swap = True
+                break
+        if not no_swap or swap:
+            if n % 2 == 0:
+                s = s[::-1] + s
+            else:
                 s += s[::-1]
-        else:
-            for i in range(n):
-                if i == 0:
-                    s = s[::-1]
-                else:
-                    s += s
         print(s)
+                
     
     
 if __name__ == "__main__":
