d3987fc57b81567d056d5755b8ba3ed176275825
Arman Jasuja <arman_jasuja@yahoo.com>
2024-08-06 07:44:36 +0400

    New Problem - Solution Accepted

---
+++ min_max_swap/min.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+        temp_a = a.copy()
+        for i in range(n):
+            temp_a[i] = min(a[i], b[i])
+            b[i] = max(a[i], b[i])
+        print(max(temp_a) * max(b))
+    
+    
+if __name__ == "__main__":
+    main()
