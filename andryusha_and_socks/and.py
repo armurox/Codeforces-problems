151f196cde94d59e95d1345b487eca50889f6c20
Arman Jasuja <arman_jasuja@yahoo.com>
2024-08-09 07:52:40 +0400

    New Problem - Solution Accepted

---
+++ andryusha_and_socks/and.py
@@ -0,0 +1,18 @@
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+    table = set()
+    count = 0
+    _max = 0
+    for elem in a:
+        if elem not in table:
+            count += 1
+            table.add(elem)
+        else:
+            count -= 1
+        _max = count if count > _max else _max
+    print(_max)
+    
+    
+if __name__ == "__main__":
+    main()
