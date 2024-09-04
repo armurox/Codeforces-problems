67e5a4cb369dee82b03c20dcca272b1afbc6068c
Arman Jasuja <arman_jasuja@yahoo.com>
2024-09-04 07:04:02 +0400

    New Problem - Solution Accepted

---
+++ turtle_and_good_strings/turtle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        cout << ((s[0] == s.back()) ? "NO\n" : "YES\n");
+    }
+}
