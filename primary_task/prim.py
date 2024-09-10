4b90a1e6a11b477853eafa75e020338f0d5cc8fc
Arman Jasuja <arman_jasuja@yahoo.com>
2024-09-10 20:02:31 +0400

    New Problem - Solution Accepted

---
+++ primary_task/prim.py
@@ -0,0 +1,17 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        size = len(n)
+        if size < 3:
+            print("NO")
+        elif n[0] == '1' and n[1] == '0' and int(n[2]) > 1:
+            print('YES')
+        elif size > 3 and n[0] == '1' and n[1] == '0' and n[2] == '1':
+            print("YES")
+        else:
+            print("NO")
+    
+    
+if __name__ == "__main__":
+    main()
