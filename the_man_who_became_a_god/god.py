f2fd5da154df5b6def77c112eca51358c07ad02d
Arman Jasuja <arman_jasuja@yahoo.com>
2024-09-24 08:12:36 +0400

    New Problem - Solution Accepted

---
+++ the_man_who_became_a_god/god.py
@@ -0,0 +1,18 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, k = map(int, input().split())
+        a = list(map(int, input().split()))
+        b = []
+        _sum = 0
+        for i in range(1, n):
+            b.append(abs(a[i] - a[i - 1]))
+            _sum += b[-1]
+        b.sort()
+        for i in range(1, k):
+            _sum -= b.pop(-1)
+        print(_sum)
+
+
+if __name__ == "__main__":
+    main()
