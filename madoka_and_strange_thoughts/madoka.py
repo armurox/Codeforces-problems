ab9ea8f70d14c0320ac97ef1d0197772ccf200fd
Arman Jasuja <arman_jasuja@yahoo.com>
2024-10-05 10:46:12 +0400

    New Problem - Solution Accepted

---
+++ madoka_and_strange_thoughts/madoka.py
@@ -0,0 +1,9 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        print(n + (2 * (n // 2)) + (2 * (n // 3)))
+    
+    
+if __name__ == "__main__":
+    main()
