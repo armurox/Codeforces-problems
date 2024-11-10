47292adaec61ca21aaebf13fa1b5ffd9cbf24489
Arman Jasuja <arman_jasuja@yahoo.com>
2024-11-10 07:27:49 +0400

    New Problem - Solution Accepted

---
+++ circuit/circuit.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        a = list(input().split())
+        num_ones = a.count('1')
+        num_zeroes = a.count('0')
+        if num_ones % 2:
+            print(1, min(num_ones, num_zeroes))
+        else:
+            print(0, min(num_ones, num_zeroes))
+
+
+if __name__ == "__main__":
+    main()
