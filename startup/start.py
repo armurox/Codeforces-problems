23f2fda939028ae2f5fdfdd0f9fa2ec9e850d0e9
Arman Jasuja <arman_jasuja@yahoo.com>
2024-11-24 07:27:20 +0400

    New Problem - Solved up until test 6 - efficient solution needed

---
+++ startup/start.py
@@ -0,0 +1,18 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, k = map(int, input().split())
+        hash_map = {}
+        for _ in range(k):
+            b, c = map(int, input().split())
+            if b in hash_map:
+                hash_map[b] += c
+            else:
+                hash_map[b] = c
+        sorted_ans = sorted(list(hash_map.values()))
+        sorted_ans.reverse()
+        print(sum(sorted_ans[:n]))
+    
+    
+if __name__ == "__main__":
+    main()
