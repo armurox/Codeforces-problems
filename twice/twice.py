1432d4eda5b2683c26b2ecfe4b38dcd827c38e0b
Arman Jasuja <arman_jasuja@yahoo.com>
2024-12-08 07:50:07 +0400

    New Problem - Solution Accepted

---
+++ twice/twice.py
@@ -0,0 +1,18 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        a = list(map(int, input().split()))
+        a_set = set()
+        num_eq = 0
+        for elem in a:
+            if elem in a_set:
+                num_eq += 2
+                a_set.remove(elem)
+            else:
+                a_set.add(elem)
+        print(num_eq // 2)
+    
+    
+if __name__ == "__main__":
+    main()
