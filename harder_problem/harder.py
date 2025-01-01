c7baebfecf2765184571c61f9f7de76378912664
Arman Jasuja <arman_jasuja@yahoo.com>
2025-01-01 10:12:10 +0400

    Slightly optimized, still upgto test 3

--- harder_problem/harder.py
+++ harder_problem/harder.py
@@ -3,26 +3,28 @@ def main():
     for _ in range(t):
         n = int(input())
         a = list(map(int, input().split()))
-        a_set = set(a)
+        a_set = list(set(a))
         used_set = set()
         final_list = []
         found = False
         for i in range(n):
-            if a[i] not in used_set:
-                final_list.append(a[i])
-                used_set.add(a[i])
-            else:
+            if a[i] in used_set:
                 for elem in a_set:
                     if elem not in used_set:
-                        found = True
-                        final_list.append(elem)
-                        used_set.add(elem)
-                        break
+                            found = True
+                            final_list.append(elem)
+                            used_set.add(elem)
+                            break
                 if not found:
                     used_set = set()
                     final_list.append(a[i])
                     used_set.add(a[i])
-                found = False
+                found = False    
+            else:
+                final_list.append(a[i])
+                used_set.add(a[i])
+            
+            
         for elem in final_list:
             print(elem, end=" ")
         print()
