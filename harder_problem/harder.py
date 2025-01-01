f3e44687687275f71acd2195424bfaea7ec4fb77
Arman Jasuja <arman_jasuja@yahoo.com>
2025-01-01 10:37:40 +0400

    Got till test 8

--- harder_problem/harder.py
+++ harder_problem/harder.py
@@ -1,29 +1,26 @@
+import random
 def main():
     t = int(input())
     for _ in range(t):
         n = int(input())
         a = list(map(int, input().split()))
-        a_set = list(set(a))
+        a_set = set(a)
         used_set = set()
+        temp_used = set()
         final_list = []
-        found = False
         for i in range(n):
+            if used_set == a_set:
+                used_set = set()
+                temp_used = set()
             if a[i] in used_set:
-                for elem in a_set:
-                    if elem not in used_set:
-                            found = True
-                            final_list.append(elem)
-                            used_set.add(elem)
-                            break
-                if not found:
-                    used_set = set()
-                    final_list.append(a[i])
-                    used_set.add(a[i])
-                found = False    
+                temp_num = random.randint(1, n)
+                while temp_num in used_set:
+                    temp_num = random.randint(1, n)
+                final_list.append(temp_num)
+                used_set.add(temp_num)
             else:
                 final_list.append(a[i])
                 used_set.add(a[i])
-            
             
         for elem in final_list:
             print(elem, end=" ")
