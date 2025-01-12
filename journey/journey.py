a9ae679fa3aeedc05bcc4821dc40dc389d6358d2
Arman Jasuja <arman_jasuja@yahoo.com>
2025-01-12 08:00:39 +0400

    Solution Accepted

--- journey/journey.py
+++ journey/journey.py
@@ -2,20 +2,17 @@ def main():
     t = int(input())
     for _ in range(t):
         n, a, b, c = map(int, input().split())
-        _sum = 0
-        days = 0
-        if a == 1 and b == 1 and c == 1:
-            print(n)
-            continue
-        while (_sum < n):
-            if days % 3 == 0:
-                _sum += a
-            elif days % 3 == 1:
-                _sum += b
-            else:
-                _sum += c
-            days += 1
-        print(days)
+        _sum = a + b + c
+        d = 3 * (n // (a + b + c))
+        if n % _sum == 0:
+            pass
+        elif n % _sum <= a:
+            d += 1
+        elif n % _sum <= a + b:
+            d += 2
+        else:
+            d += 3 
+        print(d)
     
     
 if __name__ == "__main__":
