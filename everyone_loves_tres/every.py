9a01c87ed26ee15ab8043839419d79403df79417
Arman Jasuja <arman_jasuja@yahoo.com>
2025-01-23 08:20:10 +0400

    Test Cases Added

---
+++ everyone_loves_tres/every.py
@@ -0,0 +1,19 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        if n == 1 or n == 3:
+            print(-1)
+        elif n % 2:
+            for _ in range(n - 5):
+                print(3, end="")
+            print(3633)
+        else:
+            for _ in range(n - 2):
+                print(3, end="")
+            print(66)
+            
+    
+    
+if __name__ == "__main__":
+    main()
