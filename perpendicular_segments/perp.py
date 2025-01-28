80678df888c8cbd578057d2dfaf9557acec8089d
Arman Jasuja <arman_jasuja@yahoo.com>
2025-01-28 08:10:36 +0400

    Working on solution

---
+++ perpendicular_segments/perp.py
@@ -0,0 +1,10 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        x, y, k = map(int, input().split())
+        print(0, 0, 0, k)
+        print(0, 0, k, 0)
+    
+    
+if __name__ == '__main__':
+    main()
