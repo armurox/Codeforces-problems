7a517a57a7e967a8ce0ec8a02a4c7671b8914b0a
Arman Jasuja <arman_jasuja@yahoo.com>
2025-02-04 08:13:27 +0400

    New Problem - Solution Accepted

---
+++ simple_palindrome/simple.py
@@ -0,0 +1,14 @@
+def main():
+    t = int(input())
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for _ in range(t):
+        result = []
+        n = int(input())
+        for i in range(n):
+            result.append(vowels[i % 5])
+        result.sort()
+        print(''.join(result))
+
+
+if __name__ == "__main__":
+    main()
