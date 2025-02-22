cfe4a4be05b73199d9ea6265e9ece03fb362e5d4
Arman Jasuja <arman_jasuja@yahoo.com>
2025-02-22 11:40:25 +0400

    New Problem - Solution Accepted

---
+++ forked/fork.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        a, b = map(int, input().split())
+        x_k, y_k = map(int, input().split())
+        x_q, y_q = map(int, input().split())
+        k_set = {(x_k - a, y_k - b), (x_k - b, y_k - a), (x_k - b, y_k + a), (x_k + a, y_k - b), (x_k + b, y_k - a), (x_k - a, y_k + b),
+             (x_k + a, y_k + b), (x_k + b, y_k + a)}
+        q_set = {(x_q - a, y_q - b), (x_q - b, y_q - a), (x_q - b, y_q + a), (x_q + a, y_q - b), (x_q + b, y_q - a), (x_q - a, y_q + b),
+             (x_q + a, y_q + b), (x_q + b, y_q + a)}
+        print(len(k_set.intersection(q_set)))
+
+    
+if __name__ == "__main__":
+    main()
