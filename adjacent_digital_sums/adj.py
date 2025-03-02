d20e31e8d9172203da9d18f8c3d762d584d476e1
Arman Jasuja <arman_jasuja@yahoo.com>
2025-03-02 11:43:26 +0400

    New Problem - Solution Accepted

---
+++ adjacent_digital_sums/adj.py
@@ -0,0 +1,21 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        a, b = map(int, input().split())
+        if a + 1 >= b and (a + 1 - b) % 9 == 0:
+            print('YES')
+        else:
+            print('NO')
+
+
+def sum_digits(n):
+    _sum = 0
+    while n > 0:
+        _sum += n % 10
+        n //= 10
+    return _sum
+
+    
+    
+if __name__ == "__main__":
+    main()
