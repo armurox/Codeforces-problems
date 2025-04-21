bd5ef9bc5c9bc4d3439f724e6ffdda55098f2b64
Arman Jasuja <arman_jasuja@yahoo.com>
2025-04-21 07:33:20 +0400

    New Problem - Solution Accepted

---
+++ max_and_mod/max.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        if n % 2 == 0:
+            print(-1)
+        else:
+            print(n, end=" ")
+            for x in range(1, n):
+                print(x, end=" ")
+            print()
+    
+    
+if __name__ == "__main__":
+    main()
