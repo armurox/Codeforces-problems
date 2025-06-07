7694038aaa0d0877d8e026e45e34b204619343a7
Arman Jasuja <arman_jasuja@yahoo.com>
2025-06-07 10:04:59 +0400

    New Problem - Solution Accepted

---
+++ its_time_to_duel/time.py
@@ -0,0 +1,23 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        consecutive_zeros = False
+        prev = a[0]
+        for elem in a[1:]:
+            if prev == 0 and elem == 0:
+                consecutive_zeros = True
+                break
+            else:
+                prev = elem
+            
+        if 0 not in a or consecutive_zeros:
+            print('YES')
+        else:
+            print('NO')
+        
+
+
+if __name__ == "__main__":
+    main()
