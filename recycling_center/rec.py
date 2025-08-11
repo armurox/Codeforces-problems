b50b13cb31264ccd6a2c0aa1c0e20e2390bfa388
Arman Jasuja <arman_jasuja@yahoo.com>
2025-08-11 08:49:47 +0400

    Improved

--- recycling_center/rec.py
+++ recycling_center/rec.py
@@ -5,7 +5,6 @@ def main():
         a = list(map(int, input().split()))
         a.sort(reverse=True)
         curr_multiple = 1
-        num_coins = 0
         larger_than_c = []
         for elem in a:
             if elem > c:
@@ -13,9 +12,12 @@ def main():
                 larger_than_c.append(elem)
         for elem in a:
             if elem * curr_multiple > c:
-                num_coins += 1
-            curr_multiple *= 2
-        print(num_coins + len(larger_than_c))
+                a.remove(elem)
+                larger_than_c.append(elem)
+                # print('test', elem)
+            else:
+                curr_multiple *= 2
+        print(len(larger_than_c))
     
     
 if __name__ == "__main__":
