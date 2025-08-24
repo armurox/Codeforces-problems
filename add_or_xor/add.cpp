b276a32bb06ec3046a020399e63df18352ff5806
Arman Jasuja <arman_jasuja@yahoo.com>
2025-08-24 08:30:30 +0400

    Updated solution

--- add_or_xor/add.cpp
+++ add_or_xor/add.cpp
@@ -1,34 +1,28 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
 
 int main(void)
 {
     long long t;
     std::cin >> t;
-    long long diff;
+    double diff;
     for (int i = 0; i < t; i++)
     {
         long long a, b, x, y;
         std::cin >> a >> b >> x >> y;
-        if (a > b && ((a - b) > 1 || a % 2 == 0))
+        if (a > b && (a % 2 == 0 || (a - b) > 1))
             std::cout << -1 << "\n";
-        else if (a > b && a % 2 == 1 && (a - b) == 1 and y <= x)
-            std::cout << y << "\n";
-        else if (x <= y and b > a)
-            std::cout << (b - a) * x << "\n";
-        else if ((b - a) % 2 == 0)
-        {
-            diff = (b - a) / 2;
-            std::cout << diff * y + diff * x << "\n";
-        }
-        
+        else if (a > b && a % 2 == 1 && (a - b) == 1)
+            std::cout << y << "\n";     
         else
         {
-            diff = (b - a) / 2;
+            diff = (b - a);
             if (a % 2 == 0)
-                std::cout << (diff + 1) * y + diff * x << "\n";
+                std::cout << (long long) (ceil(diff / 2) * std::min(x, y) + floor(diff / 2) * x) << "\n";
             else
-                std::cout << (diff + 1) * x + diff * y << "\n";
+                std::cout << (long long) (ceil(diff / 2) * x + floor(diff / 2) * std::min(x, y)) << "\n";
         }
     }
 }
