f4f40d6275b538b300c5c8a303f90a00a1cfa331
Arman Jasuja <arman_jasuja@yahoo.com>
2025-09-05 10:29:12 +0400

    C++ Version Of Solution Added

---
+++ cloudberry_jam/cloud.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n; 
+        cin >> n;
+        cout << 2 * n << '\n';
+    }
+}
