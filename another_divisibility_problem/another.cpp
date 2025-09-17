8b6dea40492b89a08e5d3ef038c66d85172e28ba
Arman Jasuja <arman_jasuja@yahoo.com>
2025-09-17 08:44:24 +0400

    C++ Version Of Solution Added

---
+++ another_divisibility_problem/another.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        size_t n;
+        cin >> n;
+        cout << 2 * n << '\n';
+    }
+}
