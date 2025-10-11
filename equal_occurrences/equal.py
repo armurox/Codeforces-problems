99650c72a9ebc63ebf9b1816cd47d0006c34f65e
Arman Jasuja <arman_jasuja@yahoo.com>
2025-10-11 17:51:35 +0400

    New Problem

---
+++ equal_occurrences/equal.py
@@ -0,0 +1,21 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        num_le = 1
+        curr_count = 0
+        max_balanced = 1
+        prev = a[0]
+        for elem in a[1:]:
+            if elem != prev:
+                max_balanced = max(curr_count * num_le, max_balanced)
+                num_le += 1
+                curr_count = 0
+                prev = elem
+            else:
+                curr_count += 1
+    
+    
+if __name__ == '__main__':
+    main()
