97c0d44e60a37437580e7fe559e3f23a838ea35b
Arman Jasuja <arman_jasuja@yahoo.com>
2025-10-26 12:13:53 +0400

    New Problem - Solution Accepted

---
+++ emordnilap/emordnilap.py
@@ -0,0 +1,10 @@
+import math
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        print((math.factorial(n) * n * (n - 1)) % ((10 ** 9) + 7))
+    
+    
+if __name__ == '__main__':
+    main()
