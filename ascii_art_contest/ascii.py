1a25ece8efc810e00610b9f5c05128afec36e8b0
Arman Jasuja <arman_jasuja@yahoo.com>
2025-12-06 12:41:07 +0400

    New Problem - Solution Accepted

---
+++ ascii_art_contest/ascii.py
@@ -0,0 +1,11 @@
+def main():
+    a = list(map(int, input().split()))
+    if max(a) - min(a) >= 10:
+        print('check again')
+    else:
+        a.sort()
+        print('final', a[1])
+
+    
+if __name__ == '__main__':
+    main()
