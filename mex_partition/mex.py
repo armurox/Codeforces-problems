42f45a800c1bbc4a61376333cab07eb7ae8dafa5
Arman Jasuja <arman_jasuja@yahoo.com>
2026-01-09 09:07:12 +0400

    New Problem - Solution Accepted

---
+++ mex_partition/mex.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        a = list(map(int, input().split()))
+        a.sort()
+        mex = 0
+        for elem in a:
+            if mex == elem:
+                mex += 1
+        print(mex)
+    
+    
+if __name__ == "__main__":
+    main()
