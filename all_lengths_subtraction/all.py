b12a12a810752d2dd6f4053920b1b4b9001f8c7b
Arman Jasuja <arman_jasuja@yahoo.com>
2026-01-19 09:26:07 +0400

    Working on initial solution:

---
+++ all_lengths_subtraction/all.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        max_val = max(a)
+        max_index = a.index(max_val)
+        try:
+            curr_val = a[max_index - 1]
+        except IndexError:
+            curr_val = a[max_index + 1]
+    
+    
+if __name__ == "__main__":
+    main()
