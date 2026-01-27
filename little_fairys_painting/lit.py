24765b88e48aa4df28aa52aa10a1a6f906a3c2c7
Arman Jasuja <arman_jasuja@yahoo.com>
2026-01-27 09:04:30 +0400

    New Problem - Solution Accepted

---
+++ little_fairys_painting/lit.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        a = list(map(int, input().split()))
+        initial = len(set(a))
+        a.sort()
+        for elem in a:
+            if elem >= initial:
+                print(elem)
+                break
+
+
+if __name__ == "__main__":
+    main()
