2873642e00187b2d7a8db29d6be117b7a82cbb77
Arman Jasuja <arman_jasuja@yahoo.com>
2026-03-23 08:01:56 +0400

    Solution Accepted

--- friendly_numbers/friend.py
+++ friendly_numbers/friend.py
@@ -1,16 +1,12 @@
 def main():
     t = int(input())
     for _ in range(t):
-        string_n = input()
-        n = int(string_n)
-        pow = len(string_n) if (string_n[0] == '9' or len(string_n) < 3) else len(string_n) - 1
-        # print('test', (((10**pow) * ((n // (10**pow)) + 1)) - (9 * pow), pow, n))
-        res = str(((10**pow) * ((n // (10**pow)) + 1)))
-        if n % 9 == 0 and (n < (((10**pow) * ((n // (10**pow)) + 1)) - (9 * pow)) or n == ((10**pow) * ((n // (10**pow)) + 1)) - int(res[0])):
-            print(10)
-        else:
-            print(0)
-    
+        n = int(input())
+        count = 0
+        for i in range(n, n + 82):
+            if i - sum([int(elem) for elem in str(i)]) == n:
+                count += 1
+        print(count)
 
 if __name__ == "__main__":
     main()
