1e144cde7240610a404ea25c4db1aa822f2c15ac
Arman Jasuja <arman_jasuja@yahoo.com>
2026-03-24 09:15:38 +0400

    New Problem - Solution Accepted

---
+++ sieve_of_erato67henes/sieve.py
@@ -0,0 +1,10 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        a = input().split()
+        print("YES" if "67" in a else "NO")
+    
+    
+if __name__ == "__main__":
+    main()
