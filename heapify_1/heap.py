ba017b5f9cabf8e8bcf231b21c222ed1271e0482
Arman Jasuja <arman_jasuja@yahoo.com>
2026-03-29 15:14:34 +0400

    New Problem - Test Case 1 passed

---
+++ heapify_1/heap.py
@@ -0,0 +1,20 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        if n == 1:
+            print('YES')
+            continue
+        prev = a[0]
+        count = 0
+        for i in range(1, n):
+            if a[i] < prev:
+                count += 1
+            prev = a[i]
+        print('YES' if count % 2 == 0 else 'NO')
+                
+    
+    
+if __name__ == "__main__":
+    main()
