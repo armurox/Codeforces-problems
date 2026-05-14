5e9b1fa869a71d0f1775dbca4a0d30b705b31eb2
Arman Jasuja <arman_jasuja@yahoo.com>
2026-05-14 08:48:25 +0400

    New Problem - Solution Accepted

---
+++ towers_of_boxes/tow.py
@@ -0,0 +1,13 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, m, d = map(int, input().split())
+        size_of_tower = d // m + 1
+        if n < size_of_tower:
+            print(1)
+        else:
+            print((n // size_of_tower) + (1 if n % size_of_tower != 0 else 0))
+    
+    
+if __name__ == "__main__":
+    main()
