e65782b3a48f3c4812331ac21584e0f84f80894a
Arman Jasuja <arman_jasuja@yahoo.com>
2026-05-15 08:21:41 +0400

    New Problem - Solution Accepted

---
+++ eating_game/eat.py
@@ -0,0 +1,10 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        a = list(map(int, input().split()))
+        print(a.count(max(a)))
+    
+    
+if __name__ == "__main__":
+    main()
