e70bb2178b7ef0a1c83aebbc1551da311995eda7
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-13 12:09:10 +0400

    New Problem - Solution Accepted

---
+++ palindrome_twelve_and_two_terms/pal.py
@@ -0,0 +1,16 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        if n == 10:
+            print(-1)
+        elif n % 12 == 10:
+            print(22, n - 22)
+        else:
+            print(n % 12, n - (n % 12))
+            
+            
+    
+    
+if __name__ == "__main__":
+    main()
