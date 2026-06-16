53550b7764d4f0c1ec8e3e63a2a904141bcfd5e5
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-16 09:39:09 +0400

    New Problem - Solution Accepted

---
+++ convergence/conv.py
@@ -0,0 +1,15 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        a.sort()
+        count = 0
+        for i in range(n // 2):
+            if a[i] != a[n - 1 - i]:
+                count += 1
+        print(count)
+
+
+if __name__ == "__main__":
+    main()
