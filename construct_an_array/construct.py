aa74c9b49487afc30fc64c03c09d03ae0ba67b27
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-17 09:32:14 +0400

    New Problem - Solution Accepted

---
+++ construct_an_array/construct.py
@@ -0,0 +1,13 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        start = 1
+        for _ in range(n):
+            print(start, end=" ")
+            start += 2
+        print()
+    
+    
+if __name__ == "__main__":
+    main()
