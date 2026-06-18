cbcdc9234014bac4275fc63318b334a6185232d6
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-18 09:36:25 +0400

    New Problem - Solution Accepted

---
+++ optimal_purchase/optimal.py
@@ -0,0 +1,10 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, a, b = map(int, input().split())
+        x = [(n // 3 * b) + (1 if (n % 3) else 0) * b, (n // 3 * b) + ((n % 3) * a), n * a]
+        print(min(x))
+        
+
+if __name__ == "__main__":
+    main()
