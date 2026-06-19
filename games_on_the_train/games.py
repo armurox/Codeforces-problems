a45bedf210f1103da45f643d77ee81b1617a7cc6
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-19 09:31:45 +0400

    New Problem - Solution Accepted

---
+++ games_on_the_train/games.py
@@ -0,0 +1,10 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        h = list(map(int, input().split()))
+        print((max(h) - min(h)) + 1)
+        
+    
+if __name__ == "__main__":
+    main()
