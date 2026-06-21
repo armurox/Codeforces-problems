1f0ac7c1a71710a03804a1804591a58f63c36ea0
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-21 11:53:09 +0400

    New Problem - Solution Accepted

---
+++ slimes_on_a_line/slime.py
@@ -0,0 +1,14 @@
+import math
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        min_a = min(a)
+        max_a = max(a)
+        x = (max_a + min_a) // 2
+        print(max(abs(x - min_a), abs(x - max_a)))
+    
+    
+if __name__ == "__main__":
+    main()
