229a64c63890fdbc6e2b31cb5183fe64937be532
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-26 09:33:17 +0400

    C Version Of Accepted Solution Added

---
+++ party_monster/party.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        char s[n];
+        scanf("%s", &s);
+        int bracket_count = 0;
+        for (int i = 0; i < n; i++)
+            bracket_count += (s[i] == ')') ? -1 : +1;
+        const char *ans = (bracket_count == 0) ?"YES\n": "NO\n";
+        printf("%s", ans);
+    }
+}
