815b4e054f69e150f2a3b07e1d4bae0d3849191d
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-27 11:45:09 +0400

    C Version Of Accepted Solution Added

---
+++ koshary/kosh.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        const char *ans = (x % 2 && y % 2) ? "NO\n" : "YES\n";
+        printf("%s", ans);
+    }
+}
