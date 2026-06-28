3325508f6be33688ca0a236929babe14c4c7c4e4
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-28 12:48:06 +0400

    New problem - Solution Accepted

---
+++ disturbing_distribution/disturb.py
@@ -0,0 +1,18 @@
+MAX_MOD = 676767677
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        _sum = 0
+        for i in range(n):
+            if a[i] == 1 and i != n - 1:
+                continue
+            else:
+                _sum += a[i]
+        print(_sum % MAX_MOD)
+    
+    
+if __name__ == "__main__":
+    main()
