292e7795fc93da736411ba7f8eaf1799cda9a8da
Arman Jasuja <arman_jasuja@yahoo.com>
2026-06-29 09:28:45 +0400

    C version Of Accepted Solution Added

---
+++ number_between_two_others/number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        long long x, y;
+        scanf("%lld%lld", &x, &y);
+        const char *ans = (y / x == 2) ? "NO\n" : "YES\n";
+        printf("%s", ans);
+    }
+}
