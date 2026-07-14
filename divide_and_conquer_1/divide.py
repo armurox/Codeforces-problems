34383d9827a581984f1ef83b89adaa28c62fb35e
Arman Jasuja <arman_jasuja@yahoo.com>
2026-07-14 09:31:59 +0400

    New Problem - Solution Accepted

---
+++ divide_and_conquer_1/divide.py
@@ -0,0 +1,9 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        x, y = map(int, input().split())
+        print("YES" if x % y == 0 else "NO")
+    
+    
+if __name__ == "__main__":
+    main()
