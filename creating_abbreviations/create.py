a706682443cfb56df556705dc0cf2e70960756af
Arman Jasuja <arman_jasuja@yahoo.com>
2026-08-30 11:34:12 +0400

    New Problem - Solution Accepted

---
+++ creating_abbreviations/create.py
@@ -0,0 +1,18 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, m = map(int, input().split())
+        s = ""
+        yes = True
+        for _ in range(n):
+            s += input()[0].upper()
+        for _ in range(m):
+            abbrev = input()
+            for elem in abbrev:
+                if elem not in s:
+                    yes = False
+        print("YES" if yes else "NO")
+
+
+if __name__ == "__main__":
+    main()
